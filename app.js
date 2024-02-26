dfceb7a4205efc8c2fbddd72ef3ac71becc274a1
KeisukeShishido <aya.kotori0723@gmail.com>
2024-02-26 13:52:20 +0900

    YelpCamp_Errhandling_Validation01

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express'); //expressをrequire
 const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', 
@@ -29,43 +31,53 @@ app.get('/', (req,res) => {
 });
 
 
-app.get('/campgrounds',async (req,res) => {
+app.get('/campgrounds',catchAsync (async (req,res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index',{campgrounds});
-})
+}));
 
 app.get('/campgrounds/new',(req,res) => {
     res.render('campgrounds/new');
-})
-
-app.get('/campgrounds/:id',async(req,res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show',{campground});
 });
 
-app.post('/campgrounds',async (req,res) => {
+app.get('/campgrounds/:id',catchAsync( async(req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+}));
+
+app.post('/campgrounds',catchAsync(async (req,res) => {
+    if (!req.body.campground) throw new ExpressError('不正なキャンプ場のデータです。',400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.get('/campgrounds/:id/edit',async (req,res) => {
+app.get('/campgrounds/:id/edit',catchAsync( async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground});
-});
+}));
 
 
-app.put('/campgrounds/:id', async (req,res) => {
+app.put('/campgrounds/:id', catchAsync (async (req,res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete('/campgrounds/:id',async(req,res) => {
+app.delete('/campgrounds/:id',catchAsync(async(req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
+
+app.all('*',(req,res,next) => {
+    next(new ExpressError('ページが見つかりません',404));
+});
+
+app.use((err,req,res,next) => {
+    const {statusCode = 500 ,message = '問題が起きました。'} = err;
+    res.status(statusCode).send(message);
+}); 
 
 app.listen(3000, () => {
     console.log('ポート3000でリクエスト待受中'); 
