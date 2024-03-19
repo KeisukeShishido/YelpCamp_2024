9234e2aec0cd4cf6c42f6d621f7cddd8061b898f
KeisukeShishido <aya.kotori0723@gmail.com>
2024-03-19 18:08:01 +0900

    Section53Controller_StarReview

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const User = require('../models/user');
+const users = require('../controllers/users')
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.route('/register')
+    .get(users.renderRegister )
+    .post(users.register);
 
-router.post('/register', async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registerdUser = await User.register(user, password);
-        req.login(registerdUser, err => {
-            if(err) return next(err);
-            req.flash('success', 'YelpCampへようこそ');
-            res.redirect('/campgrounds');
-        });
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-
-});
-
-router.get('/login', (req, res) => {
-    res.render('users/login');
-});
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login',keepSessionInfo: true }), (req, res) => {
-    req.flash('success', 'おかえりなさい！！');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-});
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login',keepSessionInfo: true }), users.login);
 
     // const redirectUrl = req.session.returnTo || '/campgrounds';
     // delete req.session.returnTo;
@@ -55,13 +31,7 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 //     res.redirect('/campgrounds');
 // });　動画講義通り、passportのバージョンの問題で使用不可
 
-router.get('/logout', (req, res) => {
-    req.logout((e) => {
-        if (e) { return next(e); }
-        req.flash('success', 'ログアウトしました!');
-        res.redirect('/campgrounds');
-    });
-});
+router.get('/logout', users.logout);
 
 // 成功例2
 
