aae4d927ae510aa59c2a60443ee8ef2ad820ba9b
KeisukeShishido <aya.kotori0723@gmail.com>
2024-03-25 17:27:59 +0900

    YelpCamp_Section54_02

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -49,11 +49,20 @@ module.exports.renderEditForm = async (req, res) => {
 }
 
 module.exports.updateCampground = async (req, res) => {
+    console.log(req.body);
     const { id } = req.params;
-
-    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages){
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({$pull: { images: {filename: {$in: req.body.deleteImages}}}})
+    }
     req.flash('success', 'キャンプ場を更新しました');
-    res.redirect(`/campgrounds/${camp._id}`);
+    res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.deleteCampground = async (req, res) => {
